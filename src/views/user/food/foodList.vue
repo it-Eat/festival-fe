d1be0e7609fff7ab6fb8bc9c66a78685ba07dc1f
ynghan <119781387+ynghan@users.noreply.github.com>
2025-01-12 21:31:02 +0900

    ♻️ [refactor] 먹거리 리스트 수정

--- src/views/user/food/foodList.vue
+++ src/views/user/food/foodList.vue
@@ -1,46 +1,21 @@
 <script setup>
 import backHeader from "@/components/common/backHeader.vue";
-import photoCard from "@/components/common/photoCard.vue";
-import { ref } from "vue";
-
-import charryice from "@/assets/food/charryice.jpg";
-import chicken from "@/assets/food/chicken.jpg";
-import crepe from "@/assets/food/crepe.jpg";
-import fishcake from "@/assets/food/fishcake.jpg";
-import leegane from "@/assets/food/leegane.jpg";
-import sausage from "@/assets/food/sausage.jpg";
-
-const foodList = ref([
-  { foodNum: 1, img: charryice, name: "벚꽃아이스" },
-  { foodNum: 2, img: chicken, name: "닭꼬치" },
-  { foodNum: 3, img: crepe, name: "크레페" },
-  { foodNum: 4, img: fishcake, name: "오뎅" },
-  { foodNum: 5, img: leegane, name: "이가네" },
-  { foodNum: 6, img: sausage, name: "소세지" },
-
-  { foodNum: 7, img: charryice, name: "벚꽃아이스" },
-  { foodNum: 8, img: chicken, name: "닭꼬치" },
-  { foodNum: 9, img: crepe, name: "크레페" },
-  { foodNum: 10, img: fishcake, name: "오뎅" },
-  { foodNum: 11, img: leegane, name: "이가네" },
-  { foodNum: 12, img: sausage, name: "소세지" },
-]);
+import menuItem from "@/components/common/menuItem.vue";
+import { useFoodStore } from "@/stores/food";
+const foodStore = useFoodStore();
 </script>
 
 <template>
   <backHeader></backHeader>
   <div class="play-gallery">
-    <photoCard
-      v-for="food in foodList"
-      :key="food.foodNum"
-      :item="food"
-    ></photoCard>
+    <menuItem v-for="food in foodStore.foodList" :key="food.id" :menu="food" />
   </div>
 </template>
 
 <style scoped>
 .play-gallery {
-  display: grid;
+  display: flex;
+  flex-direction: column;
   grid-template-columns: repeat(3, 1fr);
   gap: 10px;
   padding: 5px;
