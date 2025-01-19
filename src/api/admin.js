55ce1ad0ffe68f72afd113d8bdd15b7c85e90c52
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2025-01-19 19:30:02 +0900

    :sparkles: [feat] 게시판 api 연결 및 openssl 설치

--- src/api/admin.js
+++ src/api/admin.js
@@ -2,7 +2,10 @@ import { localAxios } from "@/util/http-commons";
 
 const local = localAxios();
 
-// 상점 전체 조회
+// const cookie = document.cookie;
+// const accessToken = cookie.split('; ').find(row => row.startsWith('access-token=')).split('=')[1];
+
+// 분실물 조회
 async function getLostBoards(festivalId, query) {
   const {
     page = 1,
@@ -29,4 +32,35 @@ async function getLostBoards(festivalId, query) {
   }
 }
 
-export { getLostBoards };
+// 게시판 조회
+async function getBoards(festivalId, query) {
+  const {
+    page = 1,
+    pageSize = 5,
+    orderBy = "recent",
+    order = "asc",
+    keyword = "",
+    startDate = "",
+    endDate = "",
+  } = query;
+
+  try {
+    const response = await local.get(`board/admin/${festivalId}`, {
+      // params 대신 data로 변경
+      data: {
+        page,
+        pageSize,
+        orderBy,
+        order,
+        keyword,
+        startDate,
+        endDate,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 에러:", error);
+    throw error;
+  }
+}
+export { getLostBoards, getBoards };
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+// import fs from "fs";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+
+  // https 로 바꿔주는 코드
+
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync("server.key"), // SSL 키 파일 경로
+  //     cert: fs.readFileSync("server.crt"), // SSL 인증서 파일 경로
+  //   },
+  //   port: 5173, // 원하는 포트 설정
+  //   open: true, // 개발 서버 실행 시 브라우저 자동 열기
+  // },
+});
