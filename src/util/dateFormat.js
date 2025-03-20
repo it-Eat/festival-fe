b6e78054fae66113c57503f721eb110c92a18b6a
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-03-20 21:04:57 +0900

    :bug: [fix] 날짜 형식 포맷 추가

--- src/util/dateFormat.js
+++ src/util/dateFormat.js
@@ -1,6 +1,38 @@
+// YYYYMMDD 형식의 문자열을 Date 객체로 변환
 export const dateFormat = (dateStr) => {
   const year = parseInt(dateStr.substring(0, 4));
   const month = parseInt(dateStr.substring(4, 6)) - 1;
   const day = parseInt(dateStr.substring(6, 8));
   return new Date(year, month, day);
+};
+
+// createdAt 형식의 문자열을 YYYY년 MM월 DD일 오전/오후 HH시 MM분으로 변환
+export const dateFormatWithTime = (dateStr) => {
+  const date = new Date(dateStr);
+  return date.toLocaleString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+// createdAt 형식의 문자열을 YYYY년 MM월 DD일로 변환
+export const dateFormatWithoutTime = (dateStr) => {
+  const date = new Date(dateStr);
+  return date.toLocaleString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+// createdAt 형식의 문자열을 오전/오후 HH시 MM분으로 변환
+export const dateFormatWithTime2 = (dateStr) => {
+  const date = new Date(dateStr);
+  return date.toLocaleString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 };
