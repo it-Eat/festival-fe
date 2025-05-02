0013069b6342e55581b27ab946c1c986f0787ee4
minsu <a2238737@gmail.com>
2025-05-02 18:02:11 +0900

    :sparkles: [Feat] 결제

--- src/views/user/food/mobileFoodOrder.vue
+++ src/views/user/food/mobileFoodOrder.vue
@@ -1,11 +1,93 @@
 <template>
   <div class="loadingComponent">
+    <checkModal
+      v-if="isModalOpen"
+      :title="modalConfig.title"
+      :message="modalConfig.message"
+      :confirmText="modalConfig.confirmText"
+      @confirm="handleConfirm"
+      @cancel="handleCancel"
+    />
     <p class="loadingText">결제 진행중...</p>
     <div class="loading-spinner"></div>
   </div>
 </template>
 
-<script></script>
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { onMounted, ref } from "vue";
+import checkModal from "@/components/common/checkModal.vue";
+import api from "@/api/axiosInstance";
+
+const festivalId = ref(null);
+const wishlistIds = ref(null);
+const totalPrice = ref(0);
+const payType = ref("");
+const isModalOpen = ref(false);
+
+const modalConfig = ref({
+  title: "",
+  message: ``,
+  confirmText: "",
+});
+
+const route = useRoute();
+const router = useRouter();
+
+onMounted(async () => {
+  console.log("route.query:", route.query);
+  try {
+    wishlistIds.value = route.query.wishlistIds; // "1,2,3" 형태의 문자열
+    totalPrice.value = Number(route.query.totalPrice); // 숫자로 변환
+    payType.value = route.query.payType;
+    festivalId.value = route.query.festivalId;
+
+    const payload = {
+      wishlistIds: wishlistIds.value,
+      totalPrice: totalPrice.value,
+      payType: payType.value,
+    };
+    const response = await api.post("/pay", payload);
+
+    if (response) {
+      modalConfig.value = {
+        title: "결제 완료",
+        message: `‼️화면을 캡쳐해주세요‼️\n주문 번호 : ${response.data.pay.waitingNumber}\n픽업 시간 : ${response.data.waitingTime}`,
+        confirmText: "확인",
+      };
+      isModalOpen.value = true;
+    } else {
+      modalConfig.value = {
+        title: "결제 오류",
+        message: "결제 중 오류가 발생했습니다. 다시 시도해주세요.",
+        confirmText: "",
+      };
+      isModalOpen.value = true;
+    }
+  } catch (error) {
+    console.log(error);
+    modalConfig.value = {
+      title: "결제 오류",
+      message: "결제 처리 중 오류가 발생했습니다. 다시 시도해주세요.",
+      confirmText: "",
+    };
+    isModalOpen.value = true;
+  }
+});
+const handleConfirm = () => {
+  isModalOpen.value = false;
+  router.push({
+    path: `/`,
+  });
+};
+
+const handleCancel = () => {
+  isModalOpen.value = false;
+  router.push({
+    path: `/${festivalId.value}/userHome/homeFood`,
+  });
+};
+</script>
 
 <style>
 .loadingComponent {
