7231f1afc7655416c140084efe233562a8f441dd
minsu <a2238737@gmail.com>
2025-09-08 17:04:27 +0900

    [FIx] 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "/api", // 프록시 설정에 맞게 수정
-  baseURL: "http://localhost:3001", // 프록시 설정에 맞게 수정
+  baseURL: "/api", // 프록시 설정에 맞게 수정
+  // baseURL: "http://localhost:3001", // 프록시 설정에 맞게 수정
   withCredentials: true, // 모든 요청에 쿠키 포함
 });
 
